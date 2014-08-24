962688f5469d0a87adab07deff637715c52fbc3e
Craig Kuhns <craigkuhns@gmail.com>
2014-08-24 10:11:43 -0400

    Make sure we load before any other plugins who might try to use it.

--- backbone-api.php
+++ backbone-api.php
@@ -8,6 +8,20 @@ Author: Craig Kuhns
 Author URI: http://craigkuhns.com
 */
 
+add_action("activated_plugin", "load_backbone_api_first");
+function load_backbone_api_first() {
+	// ensure path to this file is via main wp plugin path
+	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
+	$active_plugins = get_option('active_plugins');
+	$this_plugin_key = array_search($this_plugin, $active_plugins);
+	if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+		array_splice($active_plugins, $this_plugin_key, 1);
+		array_unshift($active_plugins, $this_plugin);
+		update_option('active_plugins', $active_plugins);
+	}
+}
+
 require_once 'WP-Router/wp-router.php';
 require_once 'backbone_api_model.php';
 require_once 'backbone_api_controller.php';
